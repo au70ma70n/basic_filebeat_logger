605b50f666e1312b9026926712af3adf4f4340ed
au70ma70n <au70ma70n@proton.me>
2025-08-25 17:12:15 -0500

    fix dockerfile

--- C2_Profiles/basic_logger/start.sh
+++ C2_Profiles/basic_logger/start.sh
@@ -2,7 +2,7 @@
 
 # Start Filebeat in the background
 echo "Starting Filebeat..."
-/usr/share/filebeat/filebeat -c /tmp/filebeat.yml &
+/usr/share/filebeat/filebeat -c /Mythic/filebeat_mythic_redelk.yml &
 
 # Start the Mythic logger
 echo "Starting Mythic logger..."
