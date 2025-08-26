9c1c4137cd1c282fe8979478db7f41812a0f5e1d
au70ma70n <au70ma70n@proton.me>
2025-08-26 13:02:47 -0500

    Add OnContainerStartFunction to start filebeat from Go instead of shell script

--- C2_Profiles/basic_logger/my_logger/initialize.go
+++ C2_Profiles/basic_logger/my_logger/initialize.go
@@ -1,18 +1,50 @@
 package my_logger
 
 import (
+	"os/exec"
+
 	"github.com/MythicMeta/MythicContainer/loggingstructs"
+	"github.com/MythicMeta/MythicContainer/utils/sharedStructs"
 )
 
 func Initialize() {
 	myLoggerName := "my_logger"
-	        myLogger := loggingstructs.LoggingDefinition{
-                Name:        myLoggerName,
-                Description: "basic filebeat JSON logger for RedELK",
-                LogToFilePath:  "/var/log/mythic/mythic.log",
-                LogLevel: "debug",
-                LogMaxSizeInMB: 20,
-                LogMaxBackups:  10,
+	myLogger := loggingstructs.LoggingDefinition{
+		Name:           myLoggerName,
+		Description:    "basic filebeat JSON logger for RedELK",
+		LogToFilePath:  "/var/log/mythic/mythic.log",
+		LogLevel:       "debug",
+		LogMaxSizeInMB: 20,
+		LogMaxBackups:  10,
+		OnContainerStartFunction: func(input sharedStructs.ContainerOnStartMessage) sharedStructs.ContainerOnStartMessageResponse {
+			// Start Filebeat in the background
+			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo("Starting Filebeat from Go...")
+
+			// Execute filebeat command in background
+			go func() {
+				cmd := exec.Command("/usr/share/filebeat/filebeat", "-c", "/Mythic/filebeat_mythic_redelk.yml")
+				cmd.Dir = "/Mythic"
+
+				// Start the command
+				err := cmd.Start()
+				if err != nil {
+					loggingstructs.AllLoggingData.Get(myLoggerName).LogError(err, "Failed to start Filebeat")
+					return
+				}
+
+				loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo("Filebeat started successfully", "pid", cmd.Process.Pid)
+
+				// Wait for the process to exit
+				err = cmd.Wait()
+				if err != nil {
+					loggingstructs.AllLoggingData.Get(myLoggerName).LogError(err, "Filebeat process exited with error")
+				} else {
+					loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo("Filebeat process exited normally")
+				}
+			}()
+
+			return sharedStructs.ContainerOnStartMessageResponse{}
+		},
 		NewCallbackFunction: func(input loggingstructs.NewCallbackLog) {
 			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input)
 		},
